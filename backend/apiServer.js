a0ed8074e4f22754781e5e17b014bc2ec9dfdf34
Civilight <pattarapon_ja@cmu.ac.th>
2025-05-13 16:16:46 +0700

    session7

--- backend/apiServer.js
+++ backend/apiServer.js
@@ -3,14 +3,12 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 4000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/subject', require('./routes/subject'));
-app.use('/api/contact', require('./routes/contact'));
-app.use('/api/subscribe', require('./routes/subscribe'));
+app.use('/api/register', require('./routes/register'));
 
 app.listen(PORT, () => {
     console.log('Server running at http://localhost:'+ PORT);
